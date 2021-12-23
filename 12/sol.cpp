a8a7ab6fc4abcc7bf847ba589e4515c1562dbdc9
akira <akira340@users.noreply.github.com>
2021-12-23 14:21:13 +0900

    finish 12

--- 12/sol.cpp
+++ 12/sol.cpp
@@ -1,14 +1,5 @@
-/*
-enshu 12
-67170575
-Akira Itai
- 
-gcc sol.c -lm -o a
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 double func1(double x, double y) {
   return y-12*x-3;
@@ -21,19 +12,21 @@ double euler(double (*func)(double, double), double x0, double y0, double xn, in
   double y = y0;
 
   for (int i = 1; i <= n; i++) {
+    cout << func(x, y) << '\n';
     y += func(x, y) * h;
     x = x0 + i*h;
-    // printf("%g %g\n", x, y);
+    printf("%g %g\n", x, y);
   }
   return y;
 }
 
 int main(int argc, char *argv[]) {
-  for (int n = 10; n <= 10000; n*=10) {
-    double x0 = 0.0;
-    double y0 = 1.0;
-    double xn = 1.0;
-    printf("n: %d ans: y(%g) = %g\n", n, xn, euler(func1, x0, y0, xn, n));
-  }
+  int n = 5;
+  double x0 = 0.0;
+  double y0 = 1.0;
+  double xn = 0.5;
+  
+  printf("n: %d ans: y(%g) = %g\n", n, xn, euler(func1, x0, y0, xn, n));
+
   return 0;
 }
